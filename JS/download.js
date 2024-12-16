db2ca0e7a8afb27f755644e0159522667fc184d9
Koi <rodriguez.338701@balagtas.sti.edu.ph>
2024-12-16 20:44:35 +0800

    download
    turns the .button-download to #button-download and added the function to change the text through JQuery

--- JS/download.js
+++ JS/download.js
@@ -1,4 +1,10 @@
 //JQUERY
+import $ from 'https://code.jquery.com/jquery-3.6.4.min.js';
+
 $(document).ready(function() {
-    
+    $("#button-download").click(function(){
+        $("#heading").text("Wait for your download to finish...");
+        $("#subheading").text("If your download didn't start, download again.");
+        $("#button-download").text("Download Again");
+    });
 });
