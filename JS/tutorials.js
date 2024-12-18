40123e197c733d91be7a3ef8b3a2cce07c29cd53
ennavr <princesspjvr187@gmail.com>
2024-12-19 00:10:06 +0800

    added js

--- JS/tutorials.js
+++ JS/tutorials.js
@@ -1,15 +1,18 @@
 //JQUERY
 $(document).ready(function() {
     //CODE HERE WITH JQUERY
+
+    $(".tutorial-logo").addClass("fade-in");
+
     $("p").hover(
         function() {
             $(this).css("border-color:" , "#ea680");
         },
         function(){
             $(this).css("border-color:" , "#181F23"); 
+            
        
     }
     );
-
 });
 
