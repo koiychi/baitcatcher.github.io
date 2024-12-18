bcd26b6d72b5131e7fd653e7606414806a1beaf9
ennavr <princesspjvr187@gmail.com>
2024-12-19 00:46:22 +0800

    js(animation)

--- JS/tutorials.js
+++ JS/tutorials.js
@@ -1,18 +1,30 @@
 //JQUERY
 $(document).ready(function() {
     //CODE HERE WITH JQUERY
-
     $(".tutorial-logo").addClass("fade-in");
-
+    
     $("p").hover(
         function() {
             $(this).css("border-color:" , "#ea680");
         },
         function(){
             $(this).css("border-color:" , "#181F23"); 
-            
-       
     }
     );
+
+    function moveButton(){
+        $("#button-catching").animate(
+            { left: '+=50px' }, 500,
+            function(){
+                $(this).animate(
+                    { left: '-=50px' }, 500,
+                    function(){
+                        moveButton();
+                    }
+                );
+            }
+        );
+    }
+moveButton();
 });
 
