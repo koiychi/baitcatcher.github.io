dd81adf86d81ff6199b6091b7973ec7cdd6bd562
ennavr <princesspjvr187@gmail.com>
2024-12-18 18:28:35 +0800

    tutoria-pagel(inayos ko lang onti)

--- JS/download.js
+++ JS/download.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $("#button-download").click(function(){
         $("#heading").text("Wait for your download to finish...");
         $("#subheading").text("If your download didn't start, download again.");
-        $("#button-download").text("Download Again");
+        $("#button-download").text("Download Again").css({width: '290px', height: '90px'
+        });
     });
 });
